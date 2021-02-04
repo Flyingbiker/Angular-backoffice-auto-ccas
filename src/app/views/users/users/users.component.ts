1e981204fe1cb73f2c6970d65bc9b88348f49927
Flyingbiker <schoebel.aymeric@gmail.com>
2021-02-04 10:30:12 +0100

    ajout des nums de la page sur les users

--- src/app/views/users/users/users.component.ts
+++ src/app/views/users/users/users.component.ts
@@ -14,29 +14,33 @@ export class UsersComponent implements OnInit {
   public prevLink : string |null = null;
   public nextLink : string |null = null;
 
+  public lastPage : number|null = null;
+
   constructor( private httpClient:HttpClient ) {   }
   
   ngOnInit(): void {
     //après le get les <> permet de préciser le type de données que l'on 
     //récupère. Cela permet ensuite l'autocomplétion
-    this.httpClient.get<Usercollection>('https://hb-bc-dwwm-2020.deploy.this-serv.com/api/users?page=1')
-      .subscribe(
-        (response)=>{ this.usersArray = response['hydra:member'];
-          //ou solution 2 avec une boucle for (mais plus longue)
-          //for (const user of response['hydra:member']){
-          //   this.usersArray.push(user);            
-          // }
-          // console.log(this.usersArray);  
+    this.loadPage('/api/users?page=1');
+    // code déplacé dans la méthode loadPage()
+    // this.httpClient.get<Usercollection>('https://hb-bc-dwwm-2020.deploy.this-serv.com/api/users?page=1')
+    //   .subscribe(
+    //     (response)=>{ this.usersArray = response['hydra:member'];
+    //       //ou solution 2 avec une boucle for (mais plus longue)
+    //       //for (const user of response['hydra:member']){
+    //       //   this.usersArray.push(user);            
+    //       // }
+    //       // console.log(this.usersArray);  
           
           
-          //existe-til une valeur pour le next ? 
-          if (response['hydra:view']['hydra:next'] !== undefined){
-            this.nextLink = response['hydra:view']['hydra:next'];
-          }
-        },
-        (error)=> {console.log('il y a eu une erreur de connexion ' + error);
-        }
-      );
+    //       //existe-til une valeur pour le next ? 
+    //       if (response['hydra:view']['hydra:next'] !== undefined){
+    //         this.nextLink = response['hydra:view']['hydra:next'];
+    //       }
+    //     },
+    //     (error)=> {console.log('il y a eu une erreur de connexion ' + error);
+    //     }
+    //   );
   }
 
   public loadNextPage(): void {
@@ -51,11 +55,27 @@ export class UsersComponent implements OnInit {
      } 
   }
 
+  public loadPageByNumber(pageNumber: number):void{
+    this.loadPage('/api/users?page=' + pageNumber);
+  }
+
+  public get getPageNumber(): Array<number> {
+    const arr : Array<number> = [];
+
+    if (this.lastPage !== null) {
+      for (let i = 1 ; i <this.lastPage; i++){
+        arr.push(i);      
+      }
+    }
+    return arr;
+  }
+
   private loadPage(page : string): void {
     this.httpClient.get<Usercollection>('https://hb-bc-dwwm-2020.deploy.this-serv.com'+page)
       .subscribe(
         (response)=> {
           this.usersArray = response['hydra:member'];
+          
 
           if (response['hydra:view']['hydra:next'] === undefined){
             this.nextLink = null;
@@ -67,6 +87,24 @@ export class UsersComponent implements OnInit {
             this.prevLink = null;
           } else {
             this.prevLink = response['hydra:view']['hydra:previous'];
+          }
+
+          if (response['hydra:view']['hydra:last'] === undefined){
+            this.lastPage = null;
+          } else {
+            const regex = /\?.*page=([0-9]+)/;
+            const str = response['hydra:view']['hydra:last'];
+
+            //la méthode match permet de comparer la regex avec la string
+            //ça donne un tableau avec en 2° index le retour de ce qui est entre ()
+            const matches = str.match(regex);
+
+            if (matches === null) {
+              this.lastPage = null;
+            } else {
+              this.lastPage = parseInt(matches[1]);
+            }
+
           }
         }
       );
